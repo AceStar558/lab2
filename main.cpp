be73838629578b03718387e7d1a01165131f8578
AceStar558 <48096544+AceStar558@users.noreply.github.com>
2019-03-29 16:28:15 +0300

    new _main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,19 @@ using namespace std;
 int main()
 {
 	int n,x;
-	cout<<"Введите кол-во элементов:";
-	cin>>n;
+	cout<<"Искомое число:";
 	cin>>x;
-	int a[n];
-		cout<<"Введите числа:";
-	for(int i=0; i<n; i++) {
-		cin>>a[i];
-		 if (a[i]==0)
-            break;
-		if (a[i] < 0)
-            i++;
-	}
+	do {
+		cout<<"Введите число:";
+		cin>>n;
+		if (x == n)break;
+	} while(n != 0);
 	bool f = 0;
-	for (int i=0; i<n; i++)
 	{
-		if (a[i]==x)
-		{ f = 1; break;}
+		if (x==n)
+			f = 1;
 	}
-	if (f==1) cout<<"найдено \n"; else cout<<"не найдено \n";
+	if (f) cout<<"найдено \n";
+	else cout<<"не найдено \n";
 	return 0;
 }
